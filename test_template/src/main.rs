06af28f1ce539bdce55887d0d455f6b44fdf86d6
jmli <jmli@abelian.com>
2025-10-16 22:48:35 +0800

    #Add

--- test_template/src/main.rs
+++ test_template/src/main.rs
@@ -100,10 +100,90 @@ fn test5() {
     // println!("point1: {} {}", point1.x(), point1.y()); // 所有权转移后，point1 无法再使用
 }
 
+// 定义指定了签名中所有的引用必须有相同的生命周期 'a
+fn longest<'a>(x: &'a str, y:&'a str) -> &'a str { // // 带有显式生命周期的引用 'a
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn test6() {
+    println!("===test6==================");
+    let string1 = String::from("abcd");
+    {
+        let string2 = "xyz";
+        let result = longest(string1.as_str(), string2);
+        println!("The longest string is {}", result);
+    }
+
+    let string1 = String::from("abcd");
+    let result;
+    {
+        let string2 = "xyz"; // 字符串字面值（&'static str）,生命周期是'static,程序整个运行期间都有效
+        result = longest(string1.as_str(), string2);
+    }
+    println!("The longest string is {}", result);
+
+    // let string1 = String::from("long string is long");
+    // let result;
+    // {
+    //     let string2 = String::from("xyz"); // 声明周期不是static，跳出作用域后，string2 被释放，result 无法使用
+    //     result = longest(string1.as_str(), string2.as_str());
+    // }
+    // println!("The longest string is {}", result);
+}
+
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {}", announcement);
+        self.part
+    }
+}
+fn test7() {
+    println!("===test7==================");
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt { part: first_sentence }; // 传入引用了
+    println!("i.part-> {}", i.part); 
+    let part = i.announce_and_return_part("Plllaase!!!!");
+    println!("part-> {}", part);
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+    &s[..]
+}
+
+fn test8() {
+    println!("===test8==================");
+    let string1 = String::from("hello world");
+    let result = first_word(&string1);
+    println!("The first word is: {}", result);
+}
+
+fn test9() {
+    println!("===test9==================");
+    let s:'static str = "I have a static lifetime.";
+}
 fn main() {
     test1(); // find largest number in a vector
     test2(); // 函数泛型
     test3(); // 结构体泛型
     test4(); // 枚举泛型
     test5(); // 方法泛型
+    test6(); // lifetime
+    test7(); // lifetime
+    test8(); // lifetime elision
+    test9(); // lifetime static lifetime
 }
